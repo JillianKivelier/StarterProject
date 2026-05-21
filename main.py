cb5906ac84660710a106dce99fac8efab7de4e3b
Jillian-Kivelier-HON <Jillian.Kivelier@Honeywell.com>
2026-05-21 14:19:07 -0400

    forgot to incrementally commit - set up API calls with necessary API Keys & endpoints, established detailed prompt, sent image to LLM with hard-coded URL

---
+++ main.py
@@ -0,0 +1,27 @@
+# using hard-coded URL
+from openai import AzureOpenAI
+
+client = AzureOpenAI(
+    api_key="16gB1rS6rjqJZ2POdeJuHxSOaTkZeLhI418RD70rb2R93kYVyDLkJQQJ99AKACHYHv6XJ3w3AAABACOGPq8l",   
+    api_version="2024-02-15-preview",
+    azure_endpoint="https://corp-enit-enterprise-openai.openai.azure.com/"
+)
+
+image_path = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSDMqRDiJehxNzzBrqvZP2C6r0YQ7vLHt74ia_rnEwbjg&s"
+prompt = "Generate alternative text for the image"
+
+chat_completion = client.chat.completions.create(
+    messages=[
+        
+            {"role": "user","content": [
+                {"type": "text", "text": prompt},
+                {"type": "image_url", "image_url": {"url": image_path}}
+            ]
+            },
+            {"role": "system", "content": "You are helping the visually impaired understand an image"}
+    ],
+    model="gpt-4.1-mini",
+    max_tokens=250
+)
+
+print(chat_completion.choices[0].message.content)
